a7a16f1222f6e599523a0614fbd4d33a189ccf88
JayKo <placeholder@example.com>
2025-04-14 23:28:22 +0800

    Configure for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -22,6 +22,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/codezero.jk' : '',
   experimental: {
     webpackBuildWorker: true,
     parallelServerBuildTraces: true,
